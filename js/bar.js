f5a468fcfdd9be4b3865673b5a3676a1bcfda824
mohamed-dahni <mdahni06@gmail.com>
2022-06-07 10:47:31 +0100

    initial commit

---
+++ js/bar.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    menu = document.querySelector(".menu");
+    toggle = document.querySelector(".toggle-menu");
+
+    toggle.addEventListener('click', function() {
+
+        if (menu.className === "menu") {
+            menu.className += " colapse";
+        } else {
+            menu.className = "menu";
+        }
+        
+    });
+});

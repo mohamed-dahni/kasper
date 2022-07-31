65817336dab64c74b539637cc3a538cc12bae96c
Mohamed Dahni <76598605+mohamed-dahni@users.noreply.github.com>
2022-07-31 18:27:06 +0100

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    menu = document.querySelector(".menu");
-    toggle = document.querySelector(".toggle-menu");
 
-    toggle.addEventListener('click', function() {
+menu = document.querySelector(".menu");
+toggle = document.querySelector(".toggle-menu");
 
-        if (menu.className === "menu") {
-            menu.className += " colapse";
-        } else {
-            menu.className = "menu";
-        }
-        
-    });
+toggle.addEventListener('click', function() {
+
+  if (menu.className === "menu") {
+    menu.className += " colapse";
+  } else {
+    menu.className = "menu";
+  }
+
 });
